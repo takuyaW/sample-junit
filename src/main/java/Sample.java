1dc09e870c2c5cfd1f8cea00d3b8ccb3631afe22
takuya <takuya@asial.co.jp>
2019-03-15 15:24:54 +0900

    Add gradle script

---
+++ src/main/java/Sample.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Sample {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+    @Test
+    public void testAdd() {
+        assertEquals(3, Sample.add(1, 2));
+    }
+}
